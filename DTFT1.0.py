783810d9b3cce592548d27e8ade4e72177f98122
robP <160159668+VenulaJ@users.noreply.github.com>
2025-04-04 17:32:51 -0700

    Create DTFT1.0.py

---
+++ DTFT1.0.py
@@ -0,0 +1,46 @@
+def createWindow(windowHeight, windowWidth):
+    if windowHeight && windowWidth == NULL:
+        defaultWindowSize = int(windowHeight = 300, windowWidth = 500) # measured in pixels
+    else:
+        this.windowHeight = windowHeight, this.windowWidth = windowWidth
+    backgroundColor = # FIXME insert color option here
+    windowBorder = int(windowBorderColor = putColorHere, windowBorderThickness = putSizeHere)
+    # new button minimizeButton() = createButton(putDesiredPositionHere)
+    # new button exitButton() = createButton(putDesiredPositionHere)
+    # new button nextButton() = createButton(putDesiredPositionHere)
+    # new button directoryExplorerButton() = createButton(putDesiredPositionHere)
+    return # something
+
+def createButton():
+    defaultPosition = bool(False)
+    defaultButtonSize = int(buttonHeight = 10, buttonWidth = 25) # also in pixels
+    buttonBorder = (borderColor = putColorHere, borderThickness = putSizeHere)
+    # Get user input here
+    return # something
+
+def createTextBox():
+    defaultSize = int(textBoxHeight = 10, textBoxWidth = 50) # also pixels
+    # something for accepting input
+    # what do we do with it
+    return # something
+
+def fileExplorerWindow():
+    defaultWindowSize = int(windowHeight = 300, windowWidth = 500) # measured in pixels
+    backgroundColor = # FIXME insert color option here
+    windowBorder = int(windowBorderColor = putColorHere, windowBorderThickness = putSizeHere)
+    # createExitButton()
+    # createMinimizeButton()
+    return # something
+
+def Main():
+    createWindow()
+    fileBrowserButton = bool(False)
+    chosenDirectoryPath = ''
+    while fileBrowserButton == True:
+            # Open the file browser
+            doneButton = bool(False)
+            # Get user input into chosenDirectoryPath string
+            if doneButton == True:
+                close.fileExplorerWindow()
+                return chosenDirectoryPath
+            return # chosenDirectoryPath
